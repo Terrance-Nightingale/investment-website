d5e4fe7a9e3a5a1d6622833cd22660a89541f2ed
Josh Lehr <joshua.valian17@gmail.com>
2024-04-18 14:44:39 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div id='root'>
+      <body>
+        <Header />
+      </body>
+    </div>
+  )
+}
+
+export default App
