d268e7e38a2a54252137c0b8dd1ce6531d2c84a0
Josh Lehr <joshua.valian17@gmail.com>
2024-04-18 16:24:53 -0600

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/Header';
+import UserInput from './components/UserInput';
+import Result from './components/Result';
 
 function App() {
   return (
     <div id='root'>
       <body>
         <Header />
+        <UserInput />
+        <Result />
       </body>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import headerImg from '../assets/investment-calculator-logo.png';
-import './Header.css';
 
 export default function Header() {
     return (
-        <header className='header'>
+        <header id='header'>
             <img src={headerImg} alt='bag of money'></img>
-            <h1>React Investment Calculator</h1>
+            <h1>Investment Calculator</h1>
         </header>
     );
 }
